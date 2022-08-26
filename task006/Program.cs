ba46278852e11d40f00d497c67b2bd6e6f60dff7
Dmitry Fedotov <fedotov-ds1989@yandex.ru>
2022-08-26 15:55:10 +0300

    task006

---
+++ task006/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+Console.Write(number);
+Console.Write(" - ");
+if(number%2==0)
+{
+   Console.Write("Чётное"); 
+}
+else
+{
+   Console.Write("Не чётное");
+}
