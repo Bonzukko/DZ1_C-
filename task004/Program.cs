054d6a79e5782ea232c093053e98867a19e39f56
Dmitry Fedotov <fedotov-ds1989@yandex.ru>
2022-08-26 15:28:26 +0300

    task004

---
+++ task004/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Write("Введите число a: ");
+double numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b: ");
+double numberB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число c: ");
+double numberC = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+
+Console.Write(numberA);
+Console.Write(" ");
+Console.Write(numberB);
+Console.Write(" ");
+Console.Write(numberC);
+Console.Write(" -> ");
+if(numberA > numberB)
+{
+    if(numberA > numberC)
+    {
+       Console.Write(numberA); 
+    }
+    else
+    {
+        Console.Write(numberC); 
+    }
+}
+else
+{
+    if(numberB>numberC)
+    {
+       Console.Write(numberB); 
+    }
+    else
+    {
+        Console.Write(numberC); 
+    }
+}
