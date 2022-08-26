f2cd079a70f8a7061d6e13a6efaf1a582f3da370
Dmitry Fedotov <fedotov-ds1989@yandex.ru>
2022-08-26 15:11:37 +0300

    task002

---
+++ task002/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+Console.Write("Введите число a: ");
+double numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число b: ");
+double numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("a = ");
+Console.Write(numberA);
+Console.Write(" b = ");
+Console.Write(numberB);
+
+if(numberA > numberB)
+{
+    Console.Write(" max = ");
+    Console.Write(numberA);
+}
+else
+{
+    Console.Write(" max = ");
+    Console.Write(numberB);
+}
